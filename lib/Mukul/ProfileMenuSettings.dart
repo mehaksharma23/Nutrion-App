237a28a456ffd3866643d26c1504a543442a18ff
Mukul Jindal <97662603+Mukul-Jindal@users.noreply.github.com>
2023-03-30 23:39:13 +0530

    Finished home page and Profile page

---
+++ lib/Mukul/ProfileMenuSettings.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class ProfileMenuCard extends StatelessWidget {
+  ProfileMenuCard({
+    required this.icon,
+    required this.textBox,
+    super.key,
+  });
+  IconData icon;
+  String textBox;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        height: (MediaQuery.of(context).size.height) * 0.5 * 0.15,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: (MediaQuery.of(context).size.height) *0.5 *0.15,
+              child: Row(
+                children: [
+                  Container(
+                      height: (MediaQuery.of(context).size.height) *0.5 *0.15,
+                      width: (MediaQuery.of(context).size.width) *0.15,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFFFF8EE),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Icon(
+                        icon,
+                        size: 40,
+                        color: Color(0xFFFF9385),
+                      )
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    textBox,
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Icon(
+              Icons.arrow_forward_ios,
+              color: Color(0xFF909090),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
